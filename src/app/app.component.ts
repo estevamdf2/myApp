eb65f672f31074f2a31917fece2cb9b84f67f936
Marcos Estevam de Sousa <marcos.sousa@ebc.com.br>
2018-01-03 18:35:17 -0200

    Cap 12. Aulas 1 a 7

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,12 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+
+class Cliente {
+  nome: string;
+  email: string;
+  profissao: string;
+}
 
 @Component({
   selector: 'app-root',
@@ -9,10 +17,22 @@ export class AppComponent {
   
 
   funcionarios = [];
-  
+
   //parametro funcionario passado pelo $ event no html
   aoAdicionar(funcionario){
     this.funcionarios.push(funcionario);
+  }
+
+  profissoes = ['Arquiteto', 'Engenheiro', 'Programador'];
+
+  cliente = new Cliente();
+
+  salvar(form: NgForm) {
+    this.cliente.nome = form.value.primeiroNome;
+    this.cliente.email = form.value.emailAddress;
+    this.cliente.profissao = form.value.profissao;
+    console.log(form);
+    console.log(this.cliente);
   }
 
 }
